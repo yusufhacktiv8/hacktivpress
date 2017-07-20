aa228b8aef9997a89f7078ff317acbe39d215767
myyusuf <myyusuf1911@gmail.com>
2017-07-20 14:21:17 +0700

    User sign up

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,4 +1,33 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/User');
+
+const saltRound = 10;
+const TOKEN_PASSWORD = 'mypassword';
+
+const sentLoginFailedMessage = (req, res) => {
+  res.send('Invalid username or password', 403);
+}
+
+exports.create = function createUser(req, res) {
+  const userData = req.body;
+  const password = userData.password;
+
+  bcrypt.genSalt(saltRound, (errSalt, salt) => {
+    bcrypt.hash(password, salt, (errHash, hash) => {
+      userData.password = hash;
+      const user = new User(userData);
+      user.save((err) => {
+        if (!err) {
+          res.json({ status: 'OK' });
+        } else {
+          res.status(500).send('error');
+        }
+      });
+    });
+  });
+};
 
 exports.findAll = (req, res) => {
   User.find((err, users) => {
--- server/routes/user.js
+++ server/routes/user.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const userController = require('../controllers/users');
 
 router.get('/', userController.findAll);
+router.post('/signup', userController.create);
+// router.post('/singup', userController.signup);
 
 module.exports = router;
